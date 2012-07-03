37411f0f20bddfd21ced346b1e89b612957608fa
Julio Budal <juli0w@hotmail.com>
2012-07-03 17:03:54 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juliobudal::Application.config.secret_token = '438474dd672e1e0128486ac32af2fe00f9ec7491b736ceb8481bac6e4c10dd3f91a3d2a60a1e73bdf90be32ccf7b111ca6e0f232d03f4f42c11e1b385b10b941'
