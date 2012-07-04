515d931259f8f35e250ee0068d2894a69c44ced8
Julio Budal <juli0w@hotmail.com>
2012-07-04 14:45:26 -0300

    trying cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
 require 'rvm/capistrano'
+
 set :rvm_ruby_string, 'ruby-1.9.3-p194@juliobudal'
 
-# require 'bundler/capistrano'
+load 'deploy/assets'
 
 server 'juliobudal.com', :web, :db, :app, primary: true
 set :user, "server"
@@ -22,19 +23,23 @@ after "deploy:restart", "deploy:cleanup"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
+
+after 'deploy:update_code', 'deploy:symlink_db'
+after "deploy:finalize_update", "deploy:precompile"
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-end
 
-after 'deploy:update_code', 'deploy:symlink_db'
-
-namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   end
+
+  task :precompile, :role => :app do  
+    run "cd #{release_path}/ &&  bundle exec rake assets:precompile"  
+  end  
 end
